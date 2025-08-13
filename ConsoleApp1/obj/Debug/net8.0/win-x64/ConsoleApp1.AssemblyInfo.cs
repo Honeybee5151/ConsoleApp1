5f1baa09d53d19e31208946b9348a3ca95c6c459
Shang <milessunhede258@gmail.com>
2025-08-14 00:30:34 +0200

    awdawd

--- ConsoleApp1/obj/Debug/net8.0/win-x64/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/win-x64/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c858550020e039520665fcdf5f47a4319cdb922f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2b9ea3a3081f0005ee6c2e333a9daf7322f532")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
